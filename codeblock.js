69fde8b73624c8139f2bc3d89819118ae5548001
Rockwellwindsor <rockwellwindsor@gmail.com>
2016-12-14 14:03:52 -0600

    added comments in the js file

--- codeblock.js
+++ codeblock.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-
+    // Grabs all the blocks
     var codeBlocks = document.getElementsByClassName("evo-codeblock");
-
+    // Checks for blocks
     if (codeBlocks.length > 0 ) {
+        // Loops through each block
         Array.prototype.forEach.call(codeBlocks, function() {
-            // This needs to be fixed, position cannot be trusted
+            // Grabs the identifier based on being first in order.
             var identifier = codeBlocks[0].attributes[0].nodeValue;
+            // Get the html for the div with matching identifier
             var html = $( '[data-identifier=' + identifier + ']').html();
+            // Fills the html in the target.
             $('.evo-codeblock-' + identifier + '-target pre code').text(html);
         });
     }
